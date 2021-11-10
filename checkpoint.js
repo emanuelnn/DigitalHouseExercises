3fdf0937b4a61f1b21c2b3b32c8b3a39a264836b
Emanuel N <86560624+emanuelnn@users.noreply.github.com>
2021-11-10 19:22:00 -0300

    1 ͦ Checkpoint de Programação Imperativa!

---
+++ checkpoint.js
@@ -0,0 +1,71 @@
+/* - Precisamos desenvolver um menu para um microondas super veloz, onde teremos 5 opções de comida com seus respectivos tempos pré-definidos. 
+
+      1 - Pipoca – 10 segundos (padrão);
+      2 - Macarrão – 8 segundos (padrão);
+      3 - Carne – 15 segundos (padrão);
+      4 - Feijão – 12 segundos (padrão);
+      5 - Brigadeiro – 8 segundos (padrão); 
+
+- O usuário poderá alterar o tempo padrão, aumentando ou diminuindo de acordo com sua vontade. Se o tempo informado for maior que 2x o necessário,
+ exibir mensagem que a comida queimou.
+- Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"; 
+- Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente";
+- Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
+- No final de cada tarefa, o microondas deverá exibir a mensagem: "Prato pronto, bom apetite!!!".
+*/
+
+let pipoca = 10;
+let macarrao = 8;
+let carne = 15;
+let feijao = 12;
+let brigadeiro = 8;
+
+function cozinhar(prato, tempo) {
+  // Identificar prato
+  if (
+    prato == "pipoca" ||
+    prato == "macarrao" ||
+    prato == "carne" ||
+    prato == "feijao" ||
+    prato == "brigadeiro"
+    //conferir se o prato existe
+  ) {
+    if (prato == "pipoca") {
+      prato = pipoca;
+      verificarTempo(prato, tempo);
+    } else if (prato == "macarrao") {
+      prato = macarrao;
+      verificarTempo(prato, tempo);
+    } else if (prato == "brigadeiro") {
+      prato = brigadeiro;
+      verificarTempo(prato, tempo);
+    } else if (prato == "carne") {
+      prato = carne;
+      verificarTempo(prato, tempo);
+    } else if (prato == "feijao") {
+      prato = feijao;
+      verificarTempo(prato, tempo);
+    }
+  } else
+    console.log(
+      `${prato} não está disponível em nosso padrão de tempo pré-definido.`
+    );
+  // Verificar Tempo
+  function verificarTempo(prato, tempo) {
+    if (tempo < prato) {
+      console.log("Tempo insuficiente");
+    } else if (tempo >= prato * 3) {
+      console.log("Kabumm");
+    } else if (tempo > prato * 2) {
+      console.log("A comida queimou");
+    } else if (tempo <= prato * 2) {
+      console.log("Prato pronto, bom apetite!!!");
+    }
+  }
+}
+
+cozinhar("pipoca", 5);
+cozinhar("macarrao", 10);
+cozinhar("carne", 20);
+cozinhar("feijao", 38);
+cozinhar("brigadeiro", 40);
